bb575446e9df6b4d786500464a22978bb9a2d367
Petr Mikeska <161330167+MetrPikeska@users.noreply.github.com>
2026-02-03 00:15:47 +0100

    Přesun a úprava logiky pro načítání dat z CSV a výpočet nejvyššího vrcholu

--- analyzer.py
+++ analyzer.py
@@ -1,17 +1,20 @@
 import csv
 import os
 
-
+nejvyssi_vrchol = None
+max_vyska = -1
 
 data_csv = "C:\\Users\\Metr\\Documents\\GitHub\\python-training\\beskydy.csv"
 
-def read_csv(file_path):
-    data = []
-    with open(file_path, mode='r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
-    return data
+with open(data_csv, mode='r', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        nazev = row['nazev']
+        nadmorska_vyska = int(row['nadmorska_vyska_m'])
+        
+        if nadmorska_vyska > max_vyska:
+            max_vyska = nadmorska_vyska
+            nejvyssi_vrchol = nazev
 
-print(read_csv(data_csv))
 
+print(f"nejvyssi vrchol: {nejvyssi_vrchol}, vyska: {max_vyska} m")

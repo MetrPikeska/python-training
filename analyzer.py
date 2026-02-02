72eb6b883523ebba389fd2b3f70244cef3636ee9
Petr Mikeska <161330167+MetrPikeska@users.noreply.github.com>
2026-02-03 00:16:57 +0100

    Komentáře kódů zakomentovány pro budoucí úpravy a přehlednost

--- analyzer.py
+++ analyzer.py
@@ -1,20 +1,21 @@
-import csv
-import os
+# import csv
+# import os
 
-nejvyssi_vrchol = None
-max_vyska = -1
+# nejvyssi_vrchol = None
+# max_vyska = -1
 
-data_csv = "C:\\Users\\Metr\\Documents\\GitHub\\python-training\\beskydy.csv"
+# data_csv = "C:\\Users\\Metr\\Documents\\GitHub\\python-training\\beskydy.csv"
 
-with open(data_csv, mode='r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        nazev = row['nazev']
-        nadmorska_vyska = int(row['nadmorska_vyska_m'])
+# with open(data_csv, mode='r', encoding='utf-8') as file:
+#     reader = csv.DictReader(file)
+#     for row in reader:
+#         nazev = row['nazev']
+#         nadmorska_vyska = int(row['nadmorska_vyska_m'])
         
-        if nadmorska_vyska > max_vyska:
-            max_vyska = nadmorska_vyska
-            nejvyssi_vrchol = nazev
+#         if nadmorska_vyska > max_vyska:
+#             max_vyska = nadmorska_vyska
+#             nejvyssi_vrchol = nazev
 
 
-print(f"nejvyssi vrchol: {nejvyssi_vrchol}, vyska: {max_vyska} m")
+# print(f"nejvyssi vrchol: {nejvyssi_vrchol}, vyska: {max_vyska} m")
+
